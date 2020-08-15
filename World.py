2014c39ff03eba7c7b970f5835b6f2dc54968f9c
HuXiao-THU <x-hu17@mails.tsinghua.edu.cn>
2020-08-16 00:09:48 +0800

    the world model

---
+++ World.py
@@ -0,0 +1,11 @@
+"""
+This file contains the world that the agents are in
+"""
+from Crane import Crane
+import numpy as np
+import math
+
+class World(object):
+    def __init__(self):
+        super().__init__()
+        
