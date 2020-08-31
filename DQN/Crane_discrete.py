e8e7d697421c47eb913bce997870c615335205a3
Hansen胡啸 <hansen@Huxiaos-MacBook-Pro.local>
2020-08-31 21:48:37 +0800

    runnable version

--- DQN/Crane_discrete.py
+++ DQN/Crane_discrete.py
@@ -1,5 +1,5 @@
 """
-Discrete version of a crane
+Discrete version of a crane for DQN
 """
 import math
 import numpy as np
@@ -10,3 +10,54 @@ class Crane(object):
         self.arm_omega = 0
         self.car_pos = 0
         self.hook_height = 0
+
+        self.car_limit_near_end = 3     # 小车近端距离限制
+        self.car_limit_far_end = 30     # 小车远端距离限制
+        self.height = 10                # 塔吊大臂高度，即吊钩最高高度
+
+        self.reset()
+    
+    def rotate(self, power):
+        """
+        change of speed of the arm
+
+        paras:
+        power(int): -1 to 1
+        """
+        self.arm_omega += power
+        self.arm_omega = max(-4, min(4, self.arm_omega))
+
+        self.arm_theta += self.arm_omega
+        if self.arm_theta >= 360:
+            self.arm_theta -= 360
+        if self.arm_theta < 0:
+            self.arm_theta += 360
+    
+    def moveCar(self, distance):
+        """
+        move the car by the distance
+
+        paras:
+        distance(int): -1 to 1
+        """
+        self.car_pos += distance
+        self.car_pos = max(self.car_limit_near_end, min(self.car_limit_far_end, self.car_pos))
+
+    def moveHook(self, h):
+        """
+        move the hook by h
+
+        paras:
+        h(int): -1 to 1
+        """
+        self.hook_height += h
+        self.hook_height = max(0, max(self.height, self.hook_height))
+
+    def reset(self):
+        """
+        reset the state of the crane randomly
+        """
+        self.arm_theta = np.random.randint(0, 360)
+        self.arm_omega = 0
+        self.car_pos = np.random.randint(self.car_limit_near_end, self.car_limit_far_end + 1)
+        self.hook_height = np.random.randint(0, self.height)
---
+++ DQN/train.py
@@ -0,0 +1,64 @@
+import random
+import torch
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import deque
+from World import World
+from dqn_agent import Agent
+
+
+def dqn(env, agent, n_episodes=10000, max_t=120, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    for i_episode in range(1, n_episodes+1):
+        state = env.reset()
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            next_state, reward, done, _ = env.step(action)
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break 
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=200.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), './DQN/checkpoint.pth')
+            break
+    return scores
+
+def main():
+    env = World()
+    agent = Agent(state_size=env.observation_space, action_size=env.action_space, seed=0)
+    scores = dqn(env, agent)
+    with open('./DQN/scores.pkl', 'wb') as f:
+        pickle.dump(scores, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # plot the scores
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(len(scores)), scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    fig.savefig('./DQN/training_curve.png')
+
+if __name__ == '__main__':
+    main()
